e8347f7c454dcee20e743375379c9258b99ec77b
lkri <lkri@techcollege.dk>
2023-02-16 15:03:27 +0100

    Lavede programmet mere dynamisk ved at størrelsen afhænger af antallet af navne.

--- PhilosophersEating/Program.cs
+++ PhilosophersEating/Program.cs
@@ -8,7 +8,10 @@ namespace MyApp
     internal class Program
     {
         private static string[] PhilosopherNames = { "Wittgenstein", "Nietzsche", "Platon", "Aristoteles", "Decartes" };
-        Thread[] threads = new Thread[5];
+        // programmets størrelse afhænger af at længende af PhilosopherNames.
+        Thread[] threads = new Thread[PhilosopherNames.Length];
+        private Philosopher[] philosophers = new Philosopher[PhilosopherNames.Length];
+        private Fork[] forks = new Fork[PhilosopherNames.Length];
 
         public delegate void ForkProviderMethod(Philosopher philosopher);
         ForkProviderMethod ForkProvider;
@@ -20,32 +23,25 @@ namespace MyApp
             ForkProvider = MyHelper.getForks;
         }
 
-        private Philosopher[] philosophers = new Philosopher[5];
-        private Fork[] forks = new Fork[5];
         public void go()
         {
             // Instantier 5 gafler
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < PhilosopherNames.Length; i++)
               forks[i] = new Fork();
 
-            // Instantier 5 filosoffer med hver to gafler (højre og venstre bordet rundt)
-            philosophers[0] = new Philosopher(ForkProvider, forks[0], forks[1], PhilosopherNames[0]);
-            philosophers[1] = new Philosopher(ForkProvider, forks[1], forks[2], PhilosopherNames[1]);
-            philosophers[2] = new Philosopher(ForkProvider, forks[2], forks[3], PhilosopherNames[2]);
-            philosophers[3] = new Philosopher(ForkProvider, forks[3], forks[4], PhilosopherNames[3]);
-            philosophers[4] = new Philosopher(ForkProvider, forks[4], forks[0], PhilosopherNames[4]);
-
-            // Instantier 5 tråde med filosoffernes EatMeal metode som den der kører i tråden
-            for (int i = 0; i < 5; i++)
+            for(int i = 0; i < PhilosopherNames.Length; i++)
             {
+                // Instantier 5 filosoffer med hver to gafler (højre og venstre bordet rundt)
+                philosophers[i] = new Philosopher(ForkProvider, forks[i], forks[(i+1) % 5], PhilosopherNames[i]);
+                // Instantier 5 tråde med filosoffernes EatMeal metode som den der kører i tråden
                 threads[i] = new Thread(new ThreadStart(philosophers[i].EatAMeal));
                 threads[i].Name = PhilosopherNames[i];
             }
 
-            // Start alle 5 tråde
-            for (int i = 0; i < 5; i++)
+            // Start alle tråde
+            foreach (Thread t in threads)
             {
-                threads[i].Start();
+                t.Start();
                 Thread.Sleep(300);
             }
 
